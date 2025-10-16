fb889179f3b94109f227eed90a8eb064802dec50
Naoki Takahashi <natakaha@c1r1s7.42tokyo.jp>
2025-10-16 15:45:57 +0900

    ver1

---
+++ get_next_line/get_next_line.h
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: natakaha <natakaha@student.42tokyo.jp>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/10/16 07:34:20 by natakaha          #+#    #+#             */
+/*   Updated: 2025/10/16 09:15:41 by natakaha         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <string.h>
+# include <stdlib.h>
+# include <limits.h>
+# include <stdio.h>
+# include <stdbool.h>
+# include <fcntl.h>
+# include <unistd.h>
+# include <stdint.h>
+
+# define BUFFER_SIZE  10
+
+#endif
