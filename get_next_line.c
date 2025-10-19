1c74fe67d13c353e2060e71b01cb6fad54a87d88
naoki <ai.business.naokix@gmail.com>
2025-10-19 14:05:13 +0900

    ver4

--- get_next_line.c
+++ get_next_line.c
@@ -32,7 +32,10 @@ char	*proceed_after_n(char	*str)
 	char	*tmp;
 
 	if (ft_strchr_len(str, '\n') == -1)
+	{
+		free(str);
 		return (NULL);
+	}
 	tmp = ft_strndup(str + ft_strchr_len(str, '\n') + 1,
 			ft_strchr_len(str, '\0') - ft_strchr_len(str, '\n'));
 	free(str);		
@@ -48,9 +51,8 @@ char	*read_extend_str(int fd, char *str)
 	count = BUFFER_SIZE;
 	while (ft_strchr_len(str, '\n') < 0 && count == BUFFER_SIZE)
 	{
-		count = read(fd, stack, BUFFER_SIZE);
-		if ((long long int)count < BUFFER_SIZE)
-			stack[count] = '\0';
+		count = read(fd, stack, BUFFER_SIZE);	
+		stack[count] = '\0';
 		tmp = str;
 		str = ft_strjoin(str, stack);
 		free(tmp);
@@ -77,28 +79,28 @@ char	*get_next_line(int fd)
 	return (display);
 }
 
-#include <stdio.h>
+//#include <stdio.h>
 
-int	main(int argc, char **argv)
-{
-	int		fd;
-	char	*display;
+//int	main(int argc, char **argv)
+//{
+//	int		fd;
+//	char	*display;
 
-	fd = open(argv[1], O_RDONLY);
-	display = get_next_line(fd);
-	printf("%s", display);
-	free(display);
-	display = get_next_line(fd);
-	printf("%s", display);
-	free(display);
-	display = get_next_line(fd);
-	printf("%s", display);
-	free(display);
-	display = get_next_line(fd);
-	printf("%s", display);
-	free(display);
-	(void)argc;
-}
+//	fd = open(argv[1], O_RDONLY);
+//	display = get_next_line(fd);
+//	printf("%s", display);
+//	free(display);
+//	display = get_next_line(fd);
+//	printf("%s", display);
+//	free(display);
+//	display = get_next_line(fd);
+//	printf("%s", display);
+//	free(display);
+//	display = get_next_line(fd);
+//	printf("%s", display);
+//	free(display);
+//	(void)argc;
+//}
 
 
 
